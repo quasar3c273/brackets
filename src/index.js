df1ad1e9cbcfa3211a6cc978b0f18fe3f8e4abb0
Mikhail <48333089+quasar3c273@users.noreply.github.com>
2021-03-15 10:46:00 +0300

    init: start task brackets
    init: start task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let el = bracketsConfig.map(function(element) {
+        return element.join("")
+    });
+    for (let i = 0; i < el.length; i++) {
+        if (str.includes(el[i])) {
+            str = str.replace(el[i], "");
+            i = -1;
+        }
+    }
+    return str == '';
 }
